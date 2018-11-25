3b4a6c292db3c98361d04f97fb68f7ea51e0d2ce
aescay <andrew.escay@gmail.com>
2018-11-25 23:19:50 +0800

    base landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 class Landing extends Component {
     render() {
         return(
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className='landing-grid'>
+                    <Cell col={12}>
+                        <img 
+                            src="https://png2.kisspng.com/20180425/vpe/kisspng-computer-icons-user-profile-5ae05d9102f665.4120686215246534570121.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Aspiring Data Scientist</h1>
+
+                        <hr/>
+
+                        <p>
+                            Python | R | SQL | Tableau
+                        </p>
+                        <div className="social-links">
+
+                            {/* LinkedIn */}
+                            <a href="https://www.linkedin.com/in/andrew-escay/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square" aria-hidden="true" />
+                            </a>
+
+                            {/* Github */}
+                            <a href="https://github.com/aescay" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square" aria-hidden="true" />
+                            </a>
+                        
+                        </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
