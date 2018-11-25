39b5546d455c6ad460bc8bb3230d03fc2e17e2d3
aescay <andrew.escay@gmail.com>
2018-11-26 02:55:41 +0800

    add projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,126 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+        if(this.state.activeTab === 0) {
+            return(
+                <div className='projects-grid'>
+                {/* Project 1 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background:
+                    'url(https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png) center / cover'}}>Python Project #1</CardTitle>
+                    <CardText>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    </CardText>
+                    <CardActions broder>
+                        <Button colored>GitHub</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+                </Card>
+
+                {/* Project 2 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background:
+                    'url(https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png) center / cover'}}>Python Project #2</CardTitle>
+                    <CardText>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    </CardText>
+                    <CardActions broder>
+                        <Button colored>GitHub</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+                </Card>
+
+                {/* Project 3 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background:
+                    'url(https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png) center / cover'}}>Python Project #3</CardTitle>
+                    <CardText>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    </CardText>
+                    <CardActions broder>
+                        <Button colored>GitHub</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+                </Card>
+                </div>
+            )
+        } else if (this.state.activeTab === 1) {
+            return(
+                <div className='projects-grid'>
+                {/* Project 1 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', background:
+                    'url() center / cover'}}>R Project #1</CardTitle>
+                    <CardText>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    </CardText>
+                    <CardActions broder>
+                        <Button colored>GitHub</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+                </Card>
+                </div>
+
+            )
+        } else if (this.state.activeTab === 2) {
+            return(
+                <div className='projects-grid'>
+                {/* Project 1 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: 'black', height: '176px', background:
+                    'url() center / cover'}}>React Project #1</CardTitle>
+                    <CardText>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    </CardText>
+                    <CardActions broder>
+                        <Button colored>GitHub</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share' />
+                    </CardMenu>
+                </Card>
+                </div>
+
+            )
+        }
+    }
+
     render() {
         return(
-            <div><h1>Projects Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+                    <Tab>Python</Tab>
+                    <Tab>R</Tab>
+                    <Tab>Web Development</Tab>
+                </Tabs>
+
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+            </div>
         )
     }
 }
